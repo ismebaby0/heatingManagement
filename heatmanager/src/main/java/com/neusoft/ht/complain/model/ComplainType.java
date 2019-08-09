573027954c423c1eb649f72c7579d00878016edf
rubnet <1204075579@qq.com>
2019-08-09 10:45:13 +0800

    完善 mapper的数据库缓存 model序列化 投诉类型控制类

--- heatmanager/src/main/java/com/neusoft/ht/complain/model/ComplainType.java
+++ heatmanager/src/main/java/com/neusoft/ht/complain/model/ComplainType.java
@@ -1,13 +1,15 @@
 package com.neusoft.ht.complain.model;
 
 
+import java.io.Serializable;
+
 import org.apache.ibatis.type.Alias;
 
 /*
  *   投诉类型 model 
  *   作者: 方俊坤
  */
-public class ComplainType {
+public class ComplainType implements Serializable{
     private Integer typeNo;
 
     private String typeName;
