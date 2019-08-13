7f577034aaae2cd40b7b4585ec424e93e155116e
mjg2095 <987179545@qq.com>
2019-08-13 16:31:07 +0800

    分页检索

--- heatmanager/src/main/resources/static/Admin/main.js
+++ heatmanager/src/main/resources/static/Admin/main.js
@@ -1,4 +1,67 @@
 /**
+ * 用户前端主管理JS
+ * 模块：login
+ * 业务对象:所有用户
+ * 作者:马佳国
  * 
  */
 
+$(function(){
+	var uuserid=null;
+	var upassword=null;
+	var uname=null;
+	//设置系统页面标题
+	$("span#mainpagetille").html("用户管理");
+	//设置日期的格式和选择
+	
+	//显示员工列表
+	$("table#EmployeeGrid").jqGrid({
+		url: 'user/list/page',
+		datatype: "json",
+		colModel: [
+			{ label: '用户账号', name: 'uuserid', width: 75 },
+			{ label: '用户密码', name: 'upassword', width: 90 },
+			{ label: '角色名', name: 'uname', width: 100 }                 
+		],
+		styleUI : 'Bootstrap',
+		viewrecords: true, 
+		autowidth: true,
+		height: 300,
+		rowNum: 20,
+		rowList:[10,20,30],
+		jsonReader : { 
+		      root: "list", 
+		      page: "page", 
+		      total: "pageCount", 
+		      records: "count", 
+		      repeatitems: true, 
+		      id: "id"},
+		pager: "#EmployeeGridPager"
+		
+	});
+	//更新jQGrid的列表显示
+//	function reloadEmployeeList()
+//	{
+//		$("table#EmployeeGrid").jqGrid('setGridParam',{postData:{departmentNo:departmentNo,roleNo:roleNo,sex:sex,startJoinDate:startJoinDate,endJoinDate:endJoinDate}}).trigger("reloadGrid");
+//		
+//	}
+//	
+//	//点击检索事件处理
+//	$("a#EmployeeSearchButton").on("click",function(){
+//		departmentNo=$("select#DepartmentSelection").val();
+//		roleNo=$("select#RoleSelection").val();
+//		sex=$("input[name='empsex']:checked").val();
+//		
+//		startJoinDate=$("input#startJoinDate").val();
+//		endJoinDate=$("input#endJoinDate").val();
+//		if(startJoinDate==""){
+//			startJoinDate=null;
+//		}
+//		if(endJoinDate==""){
+//			endJoinDate=null;
+//		}
+//		reloadEmployeeList();
+//	});
+	
+	
+});
