4bca7ef4eaa61472e2204a1847a97ab11516db22
rubnet <1204075579@qq.com>
2019-08-14 15:19:49 +0800

    统一居民投诉信息控制类接口

--- heatmanager/src/main/java/com/neusoft/ht/complain/controller/HomeComplainModelController.java
+++ heatmanager/src/main/java/com/neusoft/ht/complain/controller/HomeComplainModelController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.neusoft.ht.complain.model.ComplainType;
 import com.neusoft.ht.complain.model.HomeComplainModel;
 import com.neusoft.ht.complain.service.IHomeComplainModelService;
+import com.neusoft.ht.message.ResultMessage;
 
 /*
  * 居民投诉信息控制类
@@ -22,30 +24,43 @@ public class HomeComplainModelController {
 
 	// 必须参数:no 参数类型:数字
 	@RequestMapping("/get")
-	public HomeComplainModel getHomeComplainModel(Integer no) throws Exception {
-		return HomeComplainModelService.getByNo(no);
+	public ResultMessage<HomeComplainModel> getHomeComplainModel(Integer no) throws Exception {
+		ResultMessage<HomeComplainModel> result = new ResultMessage<HomeComplainModel>();
+		result.setModel(HomeComplainModelService.getByNo(no));
+		result.setStatus("200");
+		return result;
 	}
+
 	@RequestMapping("/list")
-	public List<HomeComplainModel> getAllHomeComplainModel() throws Exception {
-		return HomeComplainModelService.getByAll();
+	public ResultMessage<HomeComplainModel>  getAllHomeComplainModel() throws Exception {
+		ResultMessage<HomeComplainModel> result = new ResultMessage<HomeComplainModel>();
+		result.setList(HomeComplainModelService.getByAll());
+		result.setStatus("200");
+		return result;
 	}
 
 	// 必须参数:no 参数类型:数字
 	@RequestMapping("/delete")
-	public void delete(Integer no) throws Exception {
+	public ResultMessage<HomeComplainModel> delete(Integer no) throws Exception {
 		HomeComplainModelService.delete(no);
+		ResultMessage<HomeComplainModel> result = new ResultMessage<>("200", "删除成功");
+		return result;
 	}
 
 	/**/
 	@RequestMapping("/add")
-	public void add(HomeComplainModel record) throws Exception {
+	public ResultMessage<HomeComplainModel> add(HomeComplainModel record) throws Exception {
 		HomeComplainModelService.insert(record);
+		ResultMessage<HomeComplainModel> result = new ResultMessage<>("200", "删除成功");
+		return result;
 	}
 
 	/**/
 	@RequestMapping("/modify")
-	public void modify(HomeComplainModel record) throws Exception {
+	public ResultMessage<HomeComplainModel> modify(HomeComplainModel record) throws Exception {
 		HomeComplainModelService.updateByNo(record);
+		ResultMessage<HomeComplainModel> result = new ResultMessage<>("200", "删除成功");
+		return result;
 	}
 
 }
