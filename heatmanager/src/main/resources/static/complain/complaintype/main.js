0ead0a6283503eddba33d316d1656fa3a7b19f11
rubnet <1204075579@qq.com>
2019-08-16 10:04:10 +0800

    添加 投诉管理增加界面

--- heatmanager/src/main/resources/static/complain/complaintype/main.js
+++ heatmanager/src/main/resources/static/complain/complaintype/main.js
@@ -1,8 +1,5 @@
 /**
- * 用户前端主管理JS
- * 模块：login
- * 业务对象:所有用户
- * 作者:马佳国
+ * 用户前端主管理JS 模块：login 业务对象:所有用户 作者:马佳国
  * 
  */
 
@@ -10,17 +7,17 @@ $(function(){
 	var uuserid=null;
 	var upassword=null;
 	var uname=null;
-	//设置系统页面标题
-	$("span#mainpagetille").html("用户管理");
-	//设置日期的格式和选择
+	// 设置系统页面标题
+	$("span#mainpagetille").html("投诉管理");
+	// 设置日期的格式和选择
 	
-	//显示员工列表
+	// 显示员工列表
 	$("table#ComplainTypeGrid").jqGrid({
 		url: '/complaintype/list/page',
 		datatype: "json",
 		colModel: [
-			{ label: '类型编号', name: 'typeNo', width: 75 },
-			{ label: '类型名称', name: 'typeName', width: 90 },
+			{ label: '类型编号', name: 'typeNo', width: 50 },
+			{ label: '类型名称', name: 'typeName', width: 50 },
 			],
 		styleUI : 'Bootstrap',
 		viewrecords: true, 
@@ -39,20 +36,25 @@ $(function(){
 		
 	});
 	
-	//点击增加链接处理
+	// 点击增加链接处理
 	$("button#add").off().on("click",function(event){
+		event.preventDefault();
 		$("div#ComplainTypeDialogArea").load("complain/ComplainType/add.html",function(){
 			$("div#ComplainTypeDialogArea").dialog({
 				title:"类型名称",
 				width:600
 			});
 			$("form#addForm").ajaxForm(function(result){
-				if(result.status=="ok"){
+				$("div#ComplainTypeDialogArea").dialog("close");
+				$("div#ComplainTypeDialogArea").dialog("destroy");
+				$("div#ComplainTypeDialogArea").html("");
+				if(result.status==="OK"){
 					alert(result.message);
+					$("table#ComplainTypeGrid").trigger("reloadGrid");
 				}
 			})
 			
-			//点击取消
+			// 点击取消
 			$("input[value='取消']").on("click",function(event){
 				$("div#ComplainTypeDialogArea").dialog("close");
 				$("div#ComplainTypeDialogArea").dialog("destroy");
@@ -80,29 +82,29 @@ $(function(){
 	
 	
 	
-	//更新jQGrid的列表显示
-//	function reloadEmployeeList()
-//	{
-//		$("table#EmployeeGrid").jqGrid('setGridParam',{postData:{departmentNo:departmentNo,roleNo:roleNo,sex:sex,startJoinDate:startJoinDate,endJoinDate:endJoinDate}}).trigger("reloadGrid");
+	// 更新jQGrid的列表显示
+// function reloadEmployeeList()
+// {
+// $("table#EmployeeGrid").jqGrid('setGridParam',{postData:{departmentNo:departmentNo,roleNo:roleNo,sex:sex,startJoinDate:startJoinDate,endJoinDate:endJoinDate}}).trigger("reloadGrid");
 //		
-//	}
+// }
 //	
-//	//点击检索事件处理
-//	$("a#EmployeeSearchButton").on("click",function(){
-//		departmentNo=$("select#DepartmentSelection").val();
-//		roleNo=$("select#RoleSelection").val();
-//		sex=$("input[name='empsex']:checked").val();
+// //点击检索事件处理
+// $("a#EmployeeSearchButton").on("click",function(){
+// departmentNo=$("select#DepartmentSelection").val();
+// roleNo=$("select#RoleSelection").val();
+// sex=$("input[name='empsex']:checked").val();
 //		
-//		startJoinDate=$("input#startJoinDate").val();
-//		endJoinDate=$("input#endJoinDate").val();
-//		if(startJoinDate==""){
-//			startJoinDate=null;
-//		}
-//		if(endJoinDate==""){
-//			endJoinDate=null;
-//		}
-//		reloadEmployeeList();
-//	});
+// startJoinDate=$("input#startJoinDate").val();
+// endJoinDate=$("input#endJoinDate").val();
+// if(startJoinDate==""){
+// startJoinDate=null;
+// }
+// if(endJoinDate==""){
+// endJoinDate=null;
+// }
+// reloadEmployeeList();
+// });
 	
 	
 });
