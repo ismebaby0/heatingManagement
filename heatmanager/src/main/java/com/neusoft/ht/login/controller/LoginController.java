73246802bca31b6d8837706e5422f51ca26ff5e6
mjg <987179545@qq.com>
2019-08-22 00:03:24 +0800

    修改用户信息

--- heatmanager/src/main/java/com/neusoft/ht/login/controller/LoginController.java
+++ heatmanager/src/main/java/com/neusoft/ht/login/controller/LoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neusoft.ht.login.Service.IUserBusiness;
 
@@ -27,5 +28,12 @@ public class LoginController {
 		  
 			return "redirect:/login.html";
 		}
+	}
+	
+	@RequestMapping("/getname")
+	@ResponseBody
+	public String getName(HttpServletRequest request)throws Exception{
+		return (String) request.getSession().getAttribute("loginName");
+
 	}
 }
