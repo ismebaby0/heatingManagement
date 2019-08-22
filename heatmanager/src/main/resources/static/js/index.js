9593fbfd02d6a95138cecf5c75bbf64f3407c2e6
mjg2095 <987179545@qq.com>
2019-08-22 08:47:40 +0800

    修改

--- heatmanager/src/main/resources/static/js/index.js
+++ heatmanager/src/main/resources/static/js/index.js
@@ -2,25 +2,25 @@
  * 
  */
 var host="http://localhost:8080/";
-//$(function(){
-//	 $.get("/login/getname",function(data,status){
-//		 $('a#loginname').html(data);
-//		 console.log(data);
-//		 console.log(status);
-//		 var src="http://localhost:8080/user/image/?id="+data+"";
-//		 $("img#loginimage").attr("src",src);
-//		 console.log(src);
-//		  });
-//    
-//	 $("a#modifyUser").off().on("click",function(){
-//		 $("section#maincontent").load("modifyhtml.html",function(){
-//			 $("section#maincontent").dialog({
-//					title:"修改信息",
-//					width:800
-//				});
-//		 });
-//	 });
 $(function(){
+	 $.get("/login/getname",function(data,status){
+		 $('a#loginname').html(data);
+		 console.log(data);
+		 console.log(status);
+		 var src="http://localhost:8080/user/image/?id="+data+"";
+		 $("img#loginimage").attr("src",src);
+		 console.log(src);
+		  });
+    
+	 $("a#modifyUser").off().on("click",function(){
+		 $("section#maincontent").load("modifyhtml.html",function(){
+			 $("section#maincontent").dialog({
+					title:"修改信息",
+					width:800
+				});
+		 });
+	 });
+	 
 	//点击左面功能菜单处理
 	$("ul#menu_nav li ul li a").on("click",function(event){
 		var url=$(this).attr("href");
@@ -31,8 +31,8 @@ $(function(){
 
 		
 	});
-	
-	
-	
-	
 });
+	
+	
+	
+	
